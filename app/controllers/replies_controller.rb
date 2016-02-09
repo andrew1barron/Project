89f5e22814a32360cfb21470536682c2346d6b6e
Andrew <barrona@tcd.ie>
2016-02-09 11:55:11 +0000

    post owners can delete comments made on their posts

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,9 +2,16 @@ class RepliesController < ApplicationController
 	before_action :authorize
 	def create
 		@post = Post.find(params[:post_id])
+		#@replier = User.find(params[:user_id])
 		@reply = @post.replies.create(reply_params)
 		redirect_to post_path(@post)
 	end 
+	def destroy
+		@post = Post.find(params[:post_id])
+    	@reply = @post.replies.find(params[:id])
+    	@reply.destroy
+    	redirect_to post_path(@post)
+    end 
 
 	private
 	def reply_params
@@ -18,6 +25,10 @@ class RepliesController < ApplicationController
         #if @post && @post.user != current_user
         #redirect_to root_path, alert: "Not authorized! Only #{@post.user} has access to this post."
         #end
-      end
+      	end
     end 
+
+    #def replier
+    #	@replier = current_user.find(params[:user_id])
+    #end 
 end
