40bb9fc0381817eb4a68b5789178607a22bf067c
Andrew <barrona@tcd.ie>
2016-04-01 10:13:39 +0100

    route change didnt work removed

--- config/routes.rb
+++ config/routes.rb
@@ -28,8 +28,6 @@ Rails.application.routes.draw do
   resources :routes do
     collection do
       get :leinster do
-        get :Leinster2010
-      end 
       get :munster
       get :connacht
       get :ulster
