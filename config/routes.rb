fe375cd57898c23a0ab7b938819ace62d5409b98
Andrew <barrona@tcd.ie>
2016-04-01 12:21:12 +0100

    added route to html file

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
   resources :routes do
     collection do
       get :leinster
+      get :Leinster2010
       get :munster
       get :connacht
       get :ulster
